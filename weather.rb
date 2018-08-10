b5b6f11856877c79a6e54c6a73533ba5396ea449
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-10 13:39:57 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -27,18 +27,18 @@ def city_weather_info(weather)
 
 
 # # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal.
-# def sentence(weather)
-#   puts "The weather in #{:city} is Partly Cloudy. the temperature is #{:description} degrees Farenheit."
-# end 
+def sentence(weather)
+  puts "The weather in #{:city} is Partly Cloudy. the temperature is #{:description} degrees Farenheit."
+end 
 
 # #4. Write a method that takes in the weather hash as an argument. 
 # # You'll want to use the each method to iterate over the hash and print out
 # # `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
-# hash_each = {
-#   "The city is #{:city}",
-#   "The description is #{:description}",
-#   "The temper is #{:temperature_farenheit}"
-# }
+hash_each = {
+  "The city is #{:city}",
+  "The description is #{:description}",
+  "The temper is #{:temperature_farenheit}"
+}
   
   
 
